3292095741dc392daed2b16b62257b7795fba9d6
commandiron <commandiron@gmail.com>
2023-02-16 17:41:46 +0300

    .

--- lib/quick_post/domain/cubit/project_cubit.dart
+++ lib/quick_post/domain/cubit/project_cubit.dart
@@ -21,7 +21,7 @@ class ProjectCubit extends Cubit<ProjectState> {
   static List<Project> projects = [
     Project(
       id: "0",
-      companyLogoPath: "assets/id0/demirli_ins_logo.png",
+      companyLogoUrl: "https://firebasestorage.googleapis.com/v0/b/qrprojem.appspot.com/o/f6c423a0-adf7-11ed-afa1-0242ac120002.png?alt=media&token=9c3e9c6f-7cb3-41c2-926b-36553b77b268",
       primaryColorValue: 0xff137CC0,
       companyPhone: "+902163300300",
       companyMail: "info@demirli.com.tr",
@@ -36,12 +36,15 @@ class ProjectCubit extends Cubit<ProjectState> {
       ],
       name: "Suadiye | Oran Apartmanı",
       locationUrl: "https://goo.gl/maps/HJdvb8UVM1TnqSx69",
-      generalImagePaths: ["assets/id0/image_1.jpg", "assets/id0/image_2.jpg"],
+      generalImageUrls: [
+        "https://firebasestorage.googleapis.com/v0/b/qrprojem.appspot.com/o/d0f85d89-94c4-4e25-9935-697ac624a0e5.jpg?alt=media&token=b1f18dcc-7e9c-41a1-917c-0cee71fad7ad",
+        "https://firebasestorage.googleapis.com/v0/b/qrprojem.appspot.com/o/78b82c9b-dac2-40a1-9dba-66dae2cc8ab0.jpg?alt=media&token=50c7eede-78b3-4d83-b6a2-ae9846fb7d38"
+      ],
       apartments: [
         Apartment(
             id: 0,
             title: "8. Kat Normal Daire",
-            imagePaths: ["assets/id0/image_3.jpeg"],
+            imageUrls: ["https://firebasestorage.googleapis.com/v0/b/qrprojem.appspot.com/o/fd8b394d-7bdc-47a6-b241-50a6fea1b4e7.jpeg?alt=media&token=9df8bb3f-4cfb-4939-aa0c-ccbc30a8113b"],
             type: "5+1",
             netArea: "212",
             grossArea: "268"
