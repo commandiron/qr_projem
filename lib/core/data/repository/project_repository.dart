c66bfb575c3c95e6d8afa33ec8108f1b4d05b3f1
commandiron <commandiron@gmail.com>
2023-02-16 18:13:06 +0300

    .

--- lib/core/data/repository/project_repository.dart
+++ lib/core/data/repository/project_repository.dart
@@ -7,7 +7,6 @@ class ProjectRepository {
   final String databaseUrl =
       "https://qrprojem-default-rtdb.europe-west1.firebasedatabase.app/projects";
 
-  @override
   void insertProject(Project project) {
     final databaseUri = Uri.parse("$databaseUrl/${project.id}.json");
     http.put(databaseUri, body: json.encode(project.toJson()));
