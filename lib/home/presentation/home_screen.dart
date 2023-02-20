ec064c9ca1178e6c55115b9b97d205e0e4f09625
commandiron <commandiron@gmail.com>
2023-02-20 13:06:53 +0300

    .

--- lib/home/presentation/home_screen.dart
+++ lib/home/presentation/home_screen.dart
@@ -2,20 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qr_projem/home/domain/cubit/home_cubit.dart';
 
-import '../../core/domain/cubit/core/core_cubit.dart';
 import '../../core/presentation/widgets/nav_bar/nav_bar.dart';
 import 'home_body.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static const route = "/home";
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<HomeCubit>(
@@ -29,9 +23,5 @@ class _HomeScreenState extends State<HomeScreen> {
         )
       ),
     );
-  }
-
-  void getUser() async {
-    await BlocProvider.of<CoreCubit>(context, listen: false).getCurrentUser();
   }
 }
