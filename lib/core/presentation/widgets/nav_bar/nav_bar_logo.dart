3b656e0fb8bd1495b3020b6d4a30f65663768090
commandiron <commandiron@gmail.com>
2023-02-21 16:54:34 +0300

    .

--- lib/core/presentation/widgets/nav_bar/nav_bar_logo.dart
+++ lib/core/presentation/widgets/nav_bar/nav_bar_logo.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:qr_projem/home/presentation/home_screen.dart';
 
 class NavBarLogo extends StatelessWidget {
   const NavBarLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/images/logo/qr_projem_logo_with_text.png");
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(HomeScreen.route);
+      },
+      child: Image.asset("assets/images/logo/qr_projem_logo_with_text.png")
+    );
   }
 }
