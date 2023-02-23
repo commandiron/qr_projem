f604d5f618306d6c53583035cb478f06466463df
commandiron <commandiron@gmail.com>
2023-02-23 15:21:46 +0300

    .

--- lib/auth/presentation/sections/sign_up.dart
+++ lib/auth/presentation/sections/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qr_projem/auth/presentation/sections/sign_in.dart';
 import 'package:qr_projem/core/presentation/config/app_text_style.dart';
 
 import '../../../core/presentation/config/app_space.dart';
@@ -17,10 +18,28 @@ class SignUp extends StatelessWidget {
       child: Column(
         children: [
           AppSpace.verticalXXXL!,
-          Text("Kayıt Ol", style: AppTextStyle.h1!,),
-          AppSpace.verticalXXXL!,
+          Text("Kayıt Ol", style: AppTextStyle.h2!,),
+          AppSpace.verticalL!,
+          Text("Hesap Oluştur", style: AppTextStyle.h3!,),
+          AppSpace.verticalL!,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Zaten hesabım var.", style: AppTextStyle.h4!.copyWith(color: Colors.grey),),
+              AppSpace.horizontalS!,
+              TextButton(
+                child: Text("Giriş yap",
+                  style: AppTextStyle.h4!.copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+                onPressed: () {
+                  BlocProvider.of<AuthCubit>(context, listen: false).jumpToPage(SignIn.pageIndex);
+                },
+              )
+            ],
+          ),
+          AppSpace.verticalXL!,
           SizedBox(
-            width: 400,
+            width: 360,
             child: BlocBuilder<AuthCubit, AuthState>(
               builder: (context, state) {
                 return TextField(
