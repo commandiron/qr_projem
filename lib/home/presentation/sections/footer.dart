e9662a304b750fc4aff7e0cc0d40e36d94c24fb1
commandiron <commandiron@gmail.com>
2023-02-26 19:07:32 +0300

    .

--- lib/home/presentation/sections/footer.dart
+++ lib/home/presentation/sections/footer.dart
@@ -5,6 +5,9 @@ class Footer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 256,
+      color: Colors.grey.shade800,
+    );
   }
 }
