4a0f17a86d696b2fbc92fe3e1307b3011907efe7
commandiron <commandiron@gmail.com>
2023-03-05 14:24:59 +0300

    .

--- lib/create_new_project/presentation/steps_navigator/step_pages_navigator.dart
+++ lib/create_new_project/presentation/steps_navigator/step_pages_navigator.dart
@@ -22,45 +22,69 @@ class StepPagesNavigator extends StatelessWidget {
           bottomRight: Radius.circular(32),
         )
       ),
-      child: Row(
-        children: items.map(
-          (item) {
-            return Expanded(
-              child: Padding(
-                padding: AppPadding.allS!,
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: Row(
-                    children: [
-                      AppSpace.horizontalL!,
-                      Text(
-                        item.title,
-                        style: AppTextStyle.h3!.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Row(
+            children: items.map(
+              (item) {
+                return Expanded(
+                  child: Padding(
+                    padding: AppPadding.allS!,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Card(
+                        color: selectedIndex == item.index ? Theme.of(context).colorScheme.onPrimary : Theme.of(context).colorScheme.primary,
+                        elevation: selectedIndex == item.index ? null : 0,
+                        child: Padding(
+                          padding: AppPadding.verticalS!.add(AppPadding.horizontalM!),
+                          child: Row(
+                            children: [
+                              AppSpace.horizontalL!,
+                              Text(
+                                item.title,
+                                style: AppTextStyle.h3!.copyWith(
+                                  color: selectedIndex == item.index ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.onPrimary,
+                                )
+                              ),
+                              if (selectedIndex > item.index)
+                                Row(
+                                  children: [
+                                    AppSpace.horizontalM!,
+                                    Card(
+                                      color: const Color(0xff14B311),
+                                      margin: EdgeInsets.zero,
+                                      child: Icon(
+                                        Icons.done,
+                                        size: 24,
+                                        color: Theme.of(context).colorScheme.background,
+                                      )
+                                    )
+                                  ],
+                                ),
+                              AppSpace.horizontalL!,
+                            ],
+                          ),
                         ),
                       ),
-                      if (selectedIndex > item.index)
-                        Row(
-                          children: [
-                            AppSpace.horizontalM!,
-                            Card(
-                              color: const Color(0xff14B311),
-                              child: Icon(
-                                Icons.done,
-                                size: 24,
-                                color: Theme.of(context).colorScheme.background,
-                              )
-                            )
-                          ],
-                        ),
-                      AppSpace.horizontalL!,
-                    ],
+                    ),
                   ),
+                );
+              }
+            ).toList()
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              for (var i = 0; i < items.length - 1; i++)
+                Icon(
+                  Icons.arrow_forward,
+                  color: Theme.of(context).colorScheme.onPrimary,
                 ),
-              ),
-            );
-          }
-        ).toList()
+            ]
+          )
+        ],
       ),
     );
   }
