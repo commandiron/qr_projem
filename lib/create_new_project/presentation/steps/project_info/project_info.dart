9d0b98960837eb74389d6f6973c41b2739f9798b
commandiron <commandiron@gmail.com>
2023-03-10 15:30:50 +0300

    .

--- lib/create_new_project/presentation/steps/project_info/project_info.dart
+++ lib/create_new_project/presentation/steps/project_info/project_info.dart
@@ -4,7 +4,6 @@ import 'package:qr_projem/create_new_project/domain/create_new_project_cubit.dar
 import 'package:qr_projem/create_new_project/domain/create_new_project_state.dart';
 import 'package:qr_projem/create_new_project/presentation/widgets/project_date_picker.dart';
 import '../../../../core/presentation/config/app_padding.dart';
-import '../../../../core/presentation/config/app_space.dart';
 import '../../../../core/presentation/config/app_text_style.dart';
 
 class ProjectInfo extends StatelessWidget {
@@ -18,81 +17,74 @@ class ProjectInfo extends StatelessWidget {
       builder: (context, state) {
         return Padding(
           padding: AppPadding.horizontalXL!,
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: FractionallySizedBox(
-                  widthFactor: 0.75,
-                  child: Form(
-                    key: state.projectInfoFormKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Proje Bilgileri", style: AppTextStyle.h3),
-                        AppSpace.verticalM!,
-                        Text("Proje adı:", style: AppTextStyle.b1,),
-                        TextFormField(
-                          controller: TextEditingController(text: state.projectEntry.name),
-                          decoration: const InputDecoration(
-                            hintText: "Ör: Maltepe | Yüksel Apartmanı",
-                          ),
-                          validator: (value) {
-                            if(value == "") {
-                              return "Lütfen ilgili alanı doldurunuz.";
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) {
-                            if(newValue != null) {
-                              BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveName(newValue);
-                            }
-                          },
-                        ),
-                        AppSpace.verticalL!,
-                        ProjectDatePicker(
-                          title: "Proje Başlangıç tarihi:",
-                          dateTime: state.projectEntry.startDate,
-                          onSaved: (dateTime) {
-                            if(dateTime != null) {
-                              BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveStartDate(dateTime);
-                            }
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: FractionallySizedBox(
+              widthFactor: 0.75,
+              child: Form(
+                key: state.projectInfoFormKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(flex: 2, child: Text("Proje Bilgileri", style: AppTextStyle.h3),),
+                    Expanded(child: FittedBox(fit: BoxFit.scaleDown, child: Text("Proje adı:", style: AppTextStyle.b1,)),),
+                    Expanded(flex: 3, child: TextFormField(
+                      controller: TextEditingController(text: state.projectEntry.name),
+                      decoration: const InputDecoration(
+                        hintText: "Ör: Maltepe | Yüksel Apartmanı",
+                      ),
+                      validator: (value) {
+                        if(value == "") {
+                          return "Lütfen ilgili alanı doldurunuz.";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        if(newValue != null) {
+                          BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveName(newValue);
+                        }
+                      },
+                    ),),
+                    Expanded(child: FittedBox(fit: BoxFit.scaleDown, child: Text("Proje Başlangıç tarihi:", style: AppTextStyle.b1,)),),
+                    Expanded(flex: 3, child: ProjectDatePicker(
+                        dateTime: state.projectEntry.startDate,
+                        onSaved: (dateTime) {
+                          if(dateTime != null) {
+                            BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveStartDate(dateTime);
                           }
-                        ),
-                        AppSpace.verticalL!,
-                        ProjectDatePicker(
-                          title: "Proje Tahmini Bitiş tarihi:",
-                          dateTime: state.projectEntry.estimatedFinishDate,
-                          onSaved: (dateTime) => {
-                            if(dateTime != null) {
-                              BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveEstimatedFinishDate(dateTime),
-                            }
+                        }
+                    ),),
+                    Expanded(child: FittedBox(fit: BoxFit.scaleDown, child: Text("Proje Tahmini Bitiş tarihi:", style: AppTextStyle.b1,)),),
+                    Expanded(flex: 3, child: ProjectDatePicker(
+                        dateTime: state.projectEntry.estimatedFinishDate,
+                        onSaved: (dateTime) => {
+                          if(dateTime != null) {
+                            BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveEstimatedFinishDate(dateTime),
                           }
-                        ),
-                        AppSpace.verticalL!,
-                        TextFormField(
-                          controller: TextEditingController(text: state.projectEntry.locationUrl),
-                          decoration: const InputDecoration(
-                            hintText: "Konum: (Lütfen google haritalardan projenizin adress kordinat linkini yapıştırın)",
-                          ),
-                          validator: (value) {
-                            if(value == "") {
-                              return "Lütfen ilgili alanı doldurunuz.";
-                            }
-                            return null;
-                          },
-                          onSaved: (newValue) {
-                            if(newValue != null) {
-                              BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveLocationUrl(newValue);
-                            }
-                          },
-                        ),
-                      ]
-                    ),
-                  ),
+                        }
+                    ),),
+                    Expanded(child: FittedBox(fit: BoxFit.scaleDown, child: Text("Konum:", style: AppTextStyle.b1,)),),
+                    Expanded(flex: 3, child: TextFormField(
+                      controller: TextEditingController(text: state.projectEntry.locationUrl),
+                      decoration: const InputDecoration(
+                        hintText: "Lütfen google haritalardan projenizin adress kordinat linkini yapıştırın",
+                      ),
+                      validator: (value) {
+                        if(value == "") {
+                          return "Lütfen ilgili alanı doldurunuz.";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        if(newValue != null) {
+                          BlocProvider.of<CreateNewProjectCubit>(context, listen: false).saveLocationUrl(newValue);
+                        }
+                      },
+                    ),),
+                  ]
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         );
       },
--- lib/create_new_project/presentation/widgets/project_date_picker.dart
+++ lib/create_new_project/presentation/widgets/project_date_picker.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../../core/presentation/config/app_space.dart';
-import '../../../core/presentation/config/app_text_style.dart';
 import 'date_picker_alert_dialog.dart';
 
 class ProjectDatePicker extends StatefulWidget {
-  const ProjectDatePicker({required this.title, required this.dateTime, required this.onSaved, Key? key}) : super(key: key);
+  const ProjectDatePicker({required this.dateTime, required this.onSaved, Key? key}) : super(key: key);
 
-  final String title;
   final DateTime? dateTime;
   final void Function(DateTime? dateTime) onSaved;
 
@@ -42,43 +39,30 @@ class _ProjectDatePickerState extends State<ProjectDatePicker> {
       );
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(widget.title,  style: AppTextStyle.b1,),
-        AppSpace.verticalM!,
-        Row(
-          children: [
-            Expanded(
-              child: TextFormField(
-                controller: TextEditingController(
-                  text: widget.dateTime == null
-                    ? _selectedDateTime == null
-                      ? ""
-                      : DateFormat("dd/MM/yyyy").format(_selectedDateTime!)
-                    : DateFormat("dd/MM/yyyy").format(widget.dateTime!)
-                ),
-                readOnly: true,
-                decoration: const InputDecoration(
-                  hintText: "__/__/____",
-                ),
-                validator: (value) {
-                  if(value == "") {
-                    return "Lütfen ilgili alanı doldurunuz.";
-                  }
-                  return null;
-                },
-                onTap: () {
-                  showDatePickerDialog();
-                },
-                onSaved: (_) {
-                  widget.onSaved(_selectedDateTime);
-                },
-              ),
-            ),
-          ],
-        ),
-      ],
+    return TextFormField(
+      controller: TextEditingController(
+        text: widget.dateTime == null
+          ? _selectedDateTime == null
+            ? ""
+            : DateFormat("dd/MM/yyyy").format(_selectedDateTime!)
+          : DateFormat("dd/MM/yyyy").format(widget.dateTime!)
+      ),
+      readOnly: true,
+      decoration: const InputDecoration(
+        hintText: "__/__/____",
+      ),
+      validator: (value) {
+        if(value == "") {
+          return "Lütfen ilgili alanı doldurunuz.";
+        }
+        return null;
+      },
+      onTap: () {
+        showDatePickerDialog();
+      },
+      onSaved: (_) {
+        widget.onSaved(_selectedDateTime);
+      },
     );
   }
 }
