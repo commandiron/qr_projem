09ecec996f2d3dfd7361b4fd8b0a4647b7840105
commandiron <commandiron@gmail.com>
2023-03-12 14:45:13 +0300

    .

--- lib/generate_qr/presentation/generate_qr_view.dart
+++ lib/generate_qr/presentation/generate_qr_view.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_downloader_web/image_downloader_web.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_projem/core/presentation/config/app_padding.dart';
 import 'dart:ui' as ui;
 
 import '../../core/presentation/config/app_space.dart';
@@ -29,25 +30,31 @@ class _GenerateQrViewState extends State<GenerateQrView> {
   }
 
   Future<void> generateQrImage() async {
-    final qrImage = await getQrImage();
+
+    const exportSize = 2160;
+    const frameThickness = exportSize / 30;
+    const imageSize = exportSize - (frameThickness * 2);
+
+
+    final qrImage = await getQrImage(imageSize);
     final framedImage = await addFrameToImage(
       image: qrImage,
-      frameThickness: qrImage.width / 30,
+      frameThickness: frameThickness,
     );
-    await Future.delayed(const Duration(seconds: 2));
+    // await Future.delayed(const Duration(seconds: 2));
     setState(() {
       _qrImage = framedImage;
     });
   }
 
-  Future<ui.Image> getQrImage() async {
+  Future<ui.Image> getQrImage(double imageSize) async {
     return await QrPainter(
       data: "https://virtual.demirli.tech/",
       version: QrVersions.auto,
       gapless: true,
       color: Colors.black,
       emptyColor: Colors.white,
-    ).toImage(400);
+    ).toImage(imageSize);
   }
 
   Future<Uint8List> addFrameToImage({required ui.Image image, required double frameThickness}) async {
@@ -105,30 +112,128 @@ class _GenerateQrViewState extends State<GenerateQrView> {
       alignment: Alignment.center,
       child: Column(
         children: [
-          Container(
-            width: 400,
-            height: 400,
-            child: _qrImage == null
-                ? Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Expanded(
+            flex: 3,
+            child: Center(child: Text("Qr Kodun Hazır!", style: AppTextStyle.h2!,)),
+          ),
+          Expanded(
+            flex: 12,
+            child: Row(
               children: [
-                CircularProgressIndicator(),
-                AppSpace.verticalL!,
-                Text(
-                  "Qr kodunuz oluşturuluyor. Lütfen Bekleyiniz.",
-                  style: AppTextStyle.h3!,
-                  textAlign: TextAlign.center,
-                )
+                AppSpace.horizontalExpanded!,
+                AppSpace.horizontalExpanded!,
+                Expanded(
+                  flex: 2,
+                  child: Card(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Padding(
+                    padding: AppPadding.allL!,
+                    child: Container(
+                      width: 300,
+                      child: _qrImage == null
+                          ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircularProgressIndicator(),
+                          AppSpace.verticalL!,
+                          Text(
+                            "Qr kodunuz oluşturuluyor. Lütfen Bekleyiniz.",
+                            style: AppTextStyle.h3!,
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      )
+                          : Column(
+                        children: [
+                          Expanded(
+                            flex: 12,
+                            child: Image.memory(_qrImage!,),
+                          ),
+                          AppSpace.verticalExpanded!,
+                          Expanded(
+                            child: ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Theme.of(context).colorScheme.onPrimary
+                                    ),
+                                    foregroundColor: MaterialStatePropertyAll(
+                                        Theme.of(context).colorScheme.primary
+                                    )
+                                ),
+                                onPressed: () {
+                                  saveQrImage();
+                                },
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Bilgisayarına İndir"),
+                                    Icon(Icons.download)
+                                  ],
+                                )
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4 ,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row  (
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Durum:", style: AppTextStyle.b1!.copyWith(color: Theme.of(context).colorScheme.onPrimary),),
+                                    AppSpace.horizontalS!,
+                                    Text("Ödeme Bekleniyor.", style: AppTextStyle.b1!.copyWith(color: Theme.of(context).colorScheme.onPrimary),),
+                                  ],
+                                ),
+                                Text("Qr kodunuz ödeme sonrası aktif hale gelecektir.", style: AppTextStyle.b1!.copyWith(color: Theme.of(context).colorScheme.onPrimary),)
+                              ],
+                            )
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                ),
+                AppSpace.horizontalExpanded!,
+                Column(
+                  children: [
+                    AppSpace.verticalExpanded!,
+                    Card(
+                      color: Colors.white,
+                      child: Padding(
+                        padding: AppPadding.allL!,
+                        child: Container(
+                          width: 300,
+                          child: Expanded(
+                            child: SelectionArea(
+                              child: Column(
+                                children: [
+                                  Text("Havale/Eft Bilgileri;", style: AppTextStyle.h3b, textAlign: TextAlign.center,),
+                                  AppSpace.verticalM!,
+                                  Text("İsim Soyad: Emir Demirli", style: AppTextStyle.h4,),
+                                  AppSpace.verticalS!,
+                                  Text("Banka: Enpara - Finansbank", style: AppTextStyle.h4,),
+                                  AppSpace.verticalS!,
+                                  Text("TR12 0011 1000 0000 0066 4574 17", style: AppTextStyle.h4,),
+                                  AppSpace.verticalS!,
+                                  Text("Tutar: 1500 TL", style: AppTextStyle.h4,),
+                                ],
+                              ),
+                            )
+                          )
+                        )
+                      ),
+                    ),
+                    AppSpace.verticalExpanded!,
+                  ],
+                ),
+                AppSpace.horizontalExpanded!,
+                AppSpace.horizontalExpanded!,
               ],
             )
-                : Image.memory(_qrImage!,),
           ),
-          ElevatedButton(
-              onPressed: () {
-                saveQrImage();
-              },
-              child: Text("Save")
-          )
+          AppSpace.verticalExpanded!,
         ],
       ),
     );
